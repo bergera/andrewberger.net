4981b9900677d06bb3ab2b58ed873bed774ebdc6
Andrew Berger <andrew@andrewberger.net>
2016-07-17 14:18:36 -0400

    Update gems

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem "middleman", ">=4.0.0"
 gem "middleman-livereload"
 gem "middleman-compass", ">=4.0.0"
 gem "middleman-deploy", "=2.0.0.pre.alpha"
+gem "middleman-inliner", github: "hwhelchel/middleman-inliner"
+gem "middleman-minify-html"
 
 # Stylesheet stuff
 gem "sass"
--- config.rb
+++ config.rb
@@ -5,20 +5,27 @@ require "breakpoint"
 page "/index.html"
 
 set :haml, {format: :html5}
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+# inline CSS and JS
+activate :inliner
 
 configure :development do
   activate :livereload
 end
 
 configure :build do
+  compass_config do |config|
+    config.output_style = :compressed
+  end
+
+  activate :asset_hash
   activate :minify_css
+  activate :minify_html
   activate :relative_assets
+  activate :gzip
 end
 
 activate :deploy do |deploy|
